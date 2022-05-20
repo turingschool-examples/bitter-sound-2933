d37e229da98f31d750199bdd13d7722e20b860ba
Zachary Hazelwood <zhaze001@gmail.com>
2022-05-20 16:51:35 -0600

    test: USE 1, average year experience

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -57,4 +57,24 @@ RSpec.describe "projects#show" do
       expect(page).to have_content("Number of Contestants: 2")
     end
   end
+
+  describe "User Story Extension 1" do
+    # As a visitor,
+    # When I visit a project's show page
+    # I see the average years of experience for the contestants that worked on that project
+    it "displays the average years of experience of the contestants" do
+      recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+
+      news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+
+      jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+      gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+
+      ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+      ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+
+      visit "/projects/#{news_chic.id}"
+
+      expect(page).to have_content("Average Contestant Experience: 12.5 years")
+  end
 end
