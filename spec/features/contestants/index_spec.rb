2295bcc5c7a01210282a4e48d3c166294cd0f188
Zachary Hazelwood <zhaze001@gmail.com>
2022-05-20 17:37:43 -0600

    test: USE 2, Add contestant to project

--- spec/features/contestants/index_spec.rb
+++ spec/features/contestants/index_spec.rb
@@ -34,4 +34,44 @@ RSpec.describe "contestants#index" do
       expect(page).to_not have_content("Litfit")
     end
   end
+
+  describe "User Story Extension 2, contestants#index" do
+    it "can add a contestant to a project" do
+      recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+      furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+
+      news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+      upholstery_tux = furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+
+      jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+      gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+
+      kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+
+      ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+      ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+      ContestantProject.create(contestant_id: gretchen.id, project_id: upholstery_tux.id)
+
+      visit "/contestants"
+
+      expect(page).to have_content(jay.name)
+      expect(page).to have_content(gretchen.name)
+
+      within ("#contestant-#{kentaro.name}") do
+        expect(page).to_not have_content(news_chic.name)
+      end
+
+      visit "/projects/#{news_chic.id}"
+
+      expect(page).to have_content('Add Contestant to Project')
+      fill_in 'contestant_by_id', with: "#{kentaro.id}"
+      click_on 'Submit'
+
+      visit '/contestants'
+
+      within ("#contestant-#{kentaro.name}") do
+        expect(page).to have_content(news_chic.name)
+      end
+    end
+  end
 end
--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -78,4 +78,29 @@ RSpec.describe "projects#show" do
       expect(page).to have_content("Average Contestant Experience: 12.5 years")
     end
   end
+
+  describe "User Story Extension 2, projects#show" do
+    it "can add a contestant to a project" do
+      recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+
+      news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+
+      jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+      gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+
+      kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+
+      ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+      ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+
+      visit "/projects/#{news_chic.id}"
+
+      expect(page).to have_content('Add Contestant to Project')
+      fill_in 'contestant_by_id', with: "#{kentaro.id}"
+      click_on 'Submit'
+
+      expect(current_page).to eq("/projects/#{news_chic.id}")
+      expect(page).to have_content("Number of Contestants: 3")
+    end
+  end
 end
