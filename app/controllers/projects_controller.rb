a87f80aeef5a1d9a77f0e4cde5a8565f1b0b0b09
Bryce Wein <bryce.wein@gmail.com>
2022-05-20 17:07:32 -0400

    Refactor: abstract code away further with method

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
-    @project_theme = @project.challenge.theme
+    @project_theme = @project.challenge_theme
   end
 end
