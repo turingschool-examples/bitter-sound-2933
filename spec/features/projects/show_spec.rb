dd98f979fb6220f5b33709cd3f8f1b423567337d
Bryce Wein <bryce.wein@gmail.com>
2022-05-20 17:43:13 -0400

    Test: add a "add to project" form test

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -31,4 +31,25 @@ RSpec.describe 'projects#show', type: :feature do
     visit "/projects/#{news_chic[:id]}"
     expect(page).to have_content('Average Contestant Experience: 12.5')
   end
+
+  it 'has a form to add a contestant to the project' do
+    recycled_material_challenge = Challenge.create(theme: 'Recycled Material', project_budget: 1000)
+    news_chic = recycled_material_challenge.projects.create(name: 'News Chic', material: 'Newspaper')
+    jay = Contestant.create(name: 'Jay McCarroll', age: 40, hometown: 'LA', years_of_experience: 13)
+    gretchen = Contestant.create(name: 'Gretchen Jones', age: 36, hometown: 'NYC', years_of_experience: 12)
+    bryce = Contestant.create(name: 'Bryce Wein', age: 29, hometown: 'Gulf Breeze', years_of_experience: 29)
+    ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+    ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+    visit "/projects/#{news_chic[:id]}"
+    expect(page).to have_content('Number of Contestants: 2')
+    expect(page).to have_field('contestant_id')
+    fill_in 'contestant_id', with: bryce.id
+    click_on 'Add Contestant To Project'
+    expect(current_path).to eq("/projects/#{news_chic[:id]}")
+    expect(page).to have_content('Number of Contestants: 3')
+    visit '/contestants'
+    within("#contestant-#{bryce.id}") do
+      expect(page).to have_content(news_chic.name)
+    end
+  end
 end
