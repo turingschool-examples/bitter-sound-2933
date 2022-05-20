1d58a791194a0aa84c7550c8e69e6cd0bd464b13
Antonio Salmeron <mr.tonyboi@gmail.com>
2022-05-20 13:13:59 -0700

    Add: project show page displays contestant count  using new #contestant_count project model method

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -13,6 +13,24 @@ RSpec.describe "Project Show Page", type: :feature do
             expect(page).to_not have_content("Project Name: Boardfit")
             expect(page).to_not have_content("Material: Cardboard Boxes")
         end
+
+        it 'displays project contestant count' do
+            recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+            furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+            news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+            lit_fit = furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+            jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+            gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+            kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+            ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+            ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+            ContestantProject.create(contestant_id: jay.id, project_id: lit_fit.id)
+            ContestantProject.create(contestant_id: gretchen.id, project_id: lit_fit.id)
+            ContestantProject.create(contestant_id: kentaro.id, project_id: lit_fit.id)
+            visit "/projects/#{news_chic.id}"
+            expect(page).to have_content("Contestant Count: 2")
+            expect(page).to_not have_content("Contestant Count: 3")
+        end
     end
     
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -12,4 +12,21 @@ RSpec.describe Project, type: :model do
     it {should have_many :contestant_projects}
     it {should have_many(:contestants).through(:contestant_projects)}
   end
+
+  describe "instance methods" do
+    describe "#contestant_count" do
+      it "counts number of contestants for a given project" do
+            furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+            lit_fit = furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+            jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+            gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+            kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+            ContestantProject.create(contestant_id: jay.id, project_id: lit_fit.id)
+            ContestantProject.create(contestant_id: gretchen.id, project_id: lit_fit.id)
+            ContestantProject.create(contestant_id: kentaro.id, project_id: lit_fit.id)
+            expect(lit_fit.contestant_count).to eq(3)
+      end
+    end
+  end
+  
 end
