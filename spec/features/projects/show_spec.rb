0e42ce23de37a6d5906ef4fca901e015a69456ff
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-05-21 12:28:23 -0400

    Write test for count of contestants on project show page

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -8,28 +8,40 @@ describe 'projects show page' do
     @news_chic = @recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
     @boardfit = @recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
 
-    # upholstery_tux = furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
-    # lit_fit = furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+    @upholstery_tux = @furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+    @lit_fit = @furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
 
-    # jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
-    # gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
-    # kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
-    # erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
-    #
-    # ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
-    # ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
-    # ContestantProject.create(contestant_id: gretchen.id, project_id: upholstery_tux.id)
-    # ContestantProject.create(contestant_id: kentaro.id, project_id: upholstery_tux.id)
-    # ContestantProject.create(contestant_id: kentaro.id, project_id: boardfit.id)
-    # ContestantProject.create(contestant_id: erin.id, project_id: boardfit.id)
+    @jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    @gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+    @erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+    @kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+
+    ContestantProject.create(contestant_id: @jay.id, project_id: @news_chic.id)
+    ContestantProject.create(contestant_id: @gretchen.id, project_id: @news_chic.id)
+    ContestantProject.create(contestant_id: @kentaro.id, project_id: @news_chic.id)
+    ContestantProject.create(contestant_id: @gretchen.id, project_id: @upholstery_tux.id)
+    ContestantProject.create(contestant_id: @kentaro.id, project_id: @boardfit.id)
+    ContestantProject.create(contestant_id: @erin.id, project_id: @boardfit.id)
   end
 
   it "displays a projects name, material and theme" do
     visit "/projects/#{@boardfit.id}"
-    
+
     expect(page).to have_content("Boardfit")
     expect(page).to have_content("Cardboard Boxes")
     expect(page).to have_content("Recycled Material")
     expect(page).to_not have_content("News Chic")
+  end
+
+  it "displays the number of contestants working on this project" do
+    visit "/projects/#{@boardfit.id}"
+    expect(page).to have_content("Number of Contestants: 2")
+
+    visit "/projects/#{@upholstery_tux.id}"
+    expect(page).to have_content("Number of Contestants: 1")
+
+    visit "/projects/#{@news_chic.id}"
+    expect(page).to have_content("Number of Contestants: 3")
+
   end
 end
