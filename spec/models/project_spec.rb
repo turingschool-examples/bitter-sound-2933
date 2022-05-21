79fd6beeeeb6cd34d65ebf5d2b2a6ab4d03b8d14
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-05-21 12:08:22 -0400

    Add model test for project theme method

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -2,6 +2,14 @@ require 'rails_helper'
 
 
 RSpec.describe Project, type: :model do
+  before do
+    @recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+    @furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+
+    @lit_fit = @furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+    @boardfit = @recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+  end
+
   describe "validations" do
     it {should validate_presence_of :name}
     it {should validate_presence_of :material}
@@ -11,5 +19,13 @@ RSpec.describe Project, type: :model do
     it {should belong_to :challenge}
     it {should have_many :contestant_projects}
     it {should have_many(:contestants).through(:contestant_projects)}
+  end
+
+  describe 'instance methods' do
+    it "returns the challenge theme for an individual project" do
+      expect(@lit_fit.project_theme).to eq("Apartment Furnishings")
+      expect(@boardfit.project_theme).to eq("Recycled Material")
+
+    end
   end
 end
