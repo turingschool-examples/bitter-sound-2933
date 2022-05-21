b7c0baa45e8492a28fdb250613d911dfc8f8448a
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-05-21 12:23:55 -0400

    Pass contestant index spec tests, complete user story 2

--- spec/features/contestants/index_spec.rb
+++ spec/features/contestants/index_spec.rb
@@ -13,15 +13,13 @@ describe 'contestants index page' do
 
     @jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
     @gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
-    # kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
     @erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
 
     ContestantProject.create(contestant_id: @jay.id, project_id: @news_chic.id)
     ContestantProject.create(contestant_id: @gretchen.id, project_id: @news_chic.id)
     ContestantProject.create(contestant_id: @gretchen.id, project_id: @upholstery_tux.id)
-    # ContestantProject.create(contestant_id: kentaro.id, project_id: upholstery_tux.id)
-    # ContestantProject.create(contestant_id: kentaro.id, project_id: boardfit.id)
     ContestantProject.create(contestant_id: @erin.id, project_id: @boardfit.id)
+    
     visit '/contestants'
   end
 
@@ -36,6 +34,12 @@ describe 'contestants index page' do
       expect(page).to have_content("News Chic")
       expect(page).to have_content("Upholstery Tuxedo")
       expect(page).to_not have_content("Boardfit")
+    end
+    within "#contestant-#{@erin.id}" do
+      expect(page).to have_content("Boardfit")
+      expect(page).to_not have_content("Gretchen Jones")
+      expect(page).to_not have_content("News Chic")
+      expect(page).to_not have_content("Upholstery Tuxedo")
     end
   end
 
