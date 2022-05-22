107cd3a3657f6b3bc8c8356f6fcb149cd114737c
Luke Swenson <LukeSwenson95@outlook.com>
2022-05-22 17:16:20 -0600

    refactor: refactor project show test spec

--- spec/features/project/show_spec.rb
+++ spec/features/project/show_spec.rb
@@ -5,11 +5,11 @@ RSpec.describe 'Show Page' do
     @recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
     @furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
 
-    @news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
-    @boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+    @news_chic = @recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+    @boardfit = @recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
 
-    @upholstery_tux = furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
-    @lit_fit = furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+    @upholstery_tux = @furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+    @lit_fit = @furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
 
     @jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
     @gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
@@ -17,12 +17,12 @@ RSpec.describe 'Show Page' do
     @erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
 
 
-    @project_1 = ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
-    @project_2 = ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
-    @project_3 = ContestantProject.create(contestant_id: gretchen.id, project_id: upholstery_tux.id)
-    @project_4 = ContestantProject.create(contestant_id: kentaro.id, project_id: upholstery_tux.id)
-    @project_5 =ContestantProject.create(contestant_id: kentaro.id, project_id: boardfit.id)
-    @project_6 = ContestantProject.create(contestant_id: erin.id, project_id: boardfit.id)
+    @project_1 = ContestantProject.create(contestant_id: @jay.id, project_id: @news_chic.id)
+    @project_2 = ContestantProject.create(contestant_id: @gretchen.id, project_id: @news_chic.id)
+    @project_3 = ContestantProject.create(contestant_id: @gretchen.id, project_id: @upholstery_tux.id)
+    @project_4 = ContestantProject.create(contestant_id: @kentaro.id, project_id: @upholstery_tux.id)
+    @project_5 =ContestantProject.create(contestant_id: @kentaro.id, project_id: @boardfit.id)
+    @project_6 = ContestantProject.create(contestant_id: @erin.id, project_id: @boardfit.id)
 
   end
 
