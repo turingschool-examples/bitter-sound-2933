5dd2254bf693b6271b1535d904ef064bc4228927
tjhaines-cap <tjhaines@eckerd.edu>
2022-05-22 16:15:44 -0600

    Test: challenge theme appears on project show page

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -7,15 +7,6 @@ RSpec.describe 'projects show page' do
         news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
         boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
 
-        # jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
-        # gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
-        # kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
-        # erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
-        # ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
-        # ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
-        # ContestantProject.create(contestant_id: kentaro.id, project_id: boardfit.id)
-        # ContestantProject.create(contestant_id: erin.id, project_id: boardfit.id)
-
         visit "/projects/#{news_chic.id}"
         
         expect(page).to have_content(news_chic.name)
@@ -29,5 +20,23 @@ RSpec.describe 'projects show page' do
         expect(page).to have_content(boardfit.material)
         expect(page).to_not have_content(news_chic.name)
         expect(page).to_not have_content(news_chic.material)
+    end
+
+    it 'displays the them of the challenge that the project belongs to' do
+        recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+        news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+        boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+        jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+        gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+        kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+        erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+        ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+        ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+        ContestantProject.create(contestant_id: kentaro.id, project_id: boardfit.id)
+        ContestantProject.create(contestant_id: erin.id, project_id: boardfit.id)
+
+        visit "/projects/#{news_chic.id}"
+        
+        expect(page).to have_content(recycled_material_challenge.theme)
     end
 end
