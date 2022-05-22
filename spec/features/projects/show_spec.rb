2b659a2bcfcaea9758a1e7a3211a539a38582932
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-05-22 09:28:20 -0600

    Feat: add features test for new information on project show page

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe 'project show page', type: :feature do
             lit_fit = furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
 
             visit "/projects/#{news_chic.id}"
-            save_and_open_page
+            # save_and_open_page
             expect(page).to have_content(news_chic.name)
             expect(page).to have_content(news_chic.material)
             expect(page).to_not have_content(boardfit.name)
@@ -26,5 +26,41 @@ RSpec.describe 'project show page', type: :feature do
             expect(page).to have_content(recycled_material_challenge.theme)
             expect(page).to_not have_content(furniture_challenge.theme)
         end 
+    end
+
+    describe 'user story 3' do 
+        it 'can count the number of contestants on the page' do 
+            recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+            furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+
+            boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+
+            upholstery_tux = furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+            lit_fit = furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+
+            jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+            gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+            kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+            erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+
+
+            ContestantProject.create(contestant_id: jay.id, project_id: lit_fit.id)
+            ContestantProject.create(contestant_id: gretchen.id, project_id: lit_fit.id)
+            ContestantProject.create(contestant_id: gretchen.id, project_id: upholstery_tux.id)
+            ContestantProject.create(contestant_id: kentaro.id, project_id: lit_fit.id)
+            ContestantProject.create(contestant_id: kentaro.id, project_id: boardfit.id)
+            ContestantProject.create(contestant_id: erin.id, project_id: boardfit.id)
+
+            visit "/projects/#{lit_fit.id}"
+
+            expect(page).to have_content(lit_fit.name)
+            expect(page).to have_content(lit_fit.material)
+            expect(page).to have_content(furniture_challenge.theme)
+            expect(page).to have_content("Number of Contestants: 3")
+
+            expect(page).to_not have_content(recycled_material_challenge.theme)
+            expect(page).to_not have_content(upholstery_tux.name)
+
+        end
     end
 end
