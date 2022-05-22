a13b429d1ca8ecafe23932f3242470343dbabb35
tjhaines-cap <tjhaines@eckerd.edu>
2022-05-22 17:12:35 -0600

    Feat: add list projects method to contestant model

--- app/models/contestant.rb
+++ app/models/contestant.rb
@@ -2,4 +2,12 @@ class Contestant <ApplicationRecord
   validates_presence_of :name, :age, :hometown, :years_of_experience
   has_many :contestant_projects
   has_many :projects, through: :contestant_projects
+
+  def list_projects
+    projects.pluck(:name)
+    # binding.pry
+    # project_ids = ContestantProject.where("contestant_id = #{id}").pluck(:project_id)
+    # binding.pry
+    # Project.where(id: project_ids).order(id).pluck(:name)
+  end
 end
--- spec/models/contestant_spec.rb
+++ spec/models/contestant_spec.rb
@@ -15,15 +15,15 @@ RSpec.describe Contestant, type: :model do
   end
 
   describe 'instance methods' do
-    it 'returns list of projects for contestant' do
+    it 'returns list of project names for contestant' do
       recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
       furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
 
       news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
-      boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
-
       upholstery_tux = furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+      boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
       lit_fit = furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+
 
       jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
       gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
@@ -36,10 +36,10 @@ RSpec.describe Contestant, type: :model do
       ContestantProject.create(contestant_id: kentaro.id, project_id: boardfit.id)
       ContestantProject.create(contestant_id: erin.id, project_id: boardfit.id)
 
-      expect(jay.list_projects).to eq([news_chic])
-      expect(gretchen.list_projects).to eq([news_chic, upholstery_tux])
-      expect(kentaro.list_projects).to eq([upholstery_tux, boardfit])
-      expect(erin.list_projects).to eq([boardfit])
+      expect(jay.list_projects).to eq(["News Chic"])
+      expect(gretchen.list_projects).to eq(["News Chic", "Upholstery Tuxedo"])
+      expect(kentaro.list_projects).to eq(["Upholstery Tuxedo", "Boardfit"])
+      expect(erin.list_projects).to eq(["Boardfit"])
     end
   end
 end
