df081b2a6233ab3892d31f0cfaeed89561f752c6
Luke Swenson <LukeSwenson95@outlook.com>
2022-05-22 17:14:59 -0600

    test: test for project show page

---
+++ spec/features/project/show_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.describe 'Show Page' do
+  before :each do
+    @recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+    @furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+
+    @news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+    @boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+
+    @upholstery_tux = furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+    @lit_fit = furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+
+    @jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    @gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+    @kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+    @erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+
+
+    @project_1 = ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+    @project_2 = ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+    @project_3 = ContestantProject.create(contestant_id: gretchen.id, project_id: upholstery_tux.id)
+    @project_4 = ContestantProject.create(contestant_id: kentaro.id, project_id: upholstery_tux.id)
+    @project_5 =ContestantProject.create(contestant_id: kentaro.id, project_id: boardfit.id)
+    @project_6 = ContestantProject.create(contestant_id: erin.id, project_id: boardfit.id)
+
+  end
+
+  it "can visit show page" do
+    visit "/projects/#{@new_chic}"
+
+    expect(current_path).to eq("/projects/#{@new_chic}")
+  end
+
+  it "displays its all the projects attributes and the challenges attributes" do
+    visit "/projects/#{@new_chic}"
+
+    expect(page).to have_content("News Chic")
+    expect(page).to have_content("Newspaper")
+    expect(page).to have_content("Recycled Material")
+    expect(page).to have_content(1000)
+
+  end
+end
