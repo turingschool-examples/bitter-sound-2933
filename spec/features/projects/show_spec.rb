6bd32f8912d54710056273fa837568d88474431b
Zachary P <zachary.s.prince@outlook.com>
2022-05-23 14:03:43 -0400

    Add average contestant experience test

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -62,5 +62,23 @@ RSpec.describe 'Project show page' do
       expect(page).to have_content('Number of Contestants:')
       expect(page).to have_content('2')
     end
+
+    it 'can display the average years of experience for participating contestants' do
+      recycled_material_challenge = Challenge.create(theme: 'Recycled Material', project_budget: 1000)
+      news_chic = recycled_material_challenge.projects.create(name: 'News Chic', material: 'Newspaper')
+      jay = Contestant.create(name: 'Jay McCarroll', age: 40, hometown: 'LA', years_of_experience: 13)
+      gretchen = Contestant.create(name: 'Gretchen Jones', age: 36, hometown: 'NYC', years_of_experience: 12)
+      oscar = Contestant.create(name: 'Oscar Grouch', age: 63, hometown: 'NYC', years_of_experience: 44)
+      ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+      ContestantProject.create(contestant_id: oscar.id, project_id: news_chic.id)
+      ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+
+      visit "/projects/#{news_chic.id}"
+
+      within('div#contestantExperience') do
+        expect(page).to have_content('Average Contestant Experience:')
+        expect(page).to have_content('23')
+      end
+    end
   end
 end
