35e0201c0efb0ce25fdffe382ea9daeaf6e98c37
Luke Swenson <LukeSwenson95@outlook.com>
2022-05-22 19:25:24 -0600

    test: Created test for contestant count view

--- spec/features/project/show_spec.rb
+++ spec/features/project/show_spec.rb
@@ -27,23 +27,24 @@ RSpec.describe 'Show Page' do
   end
 
   it "can visit show page" do
-    recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
-    news_chic = @recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
-
     visit "/projects/#{@news_chic.id}"
 
     expect(current_path).to eq("/projects/#{@news_chic.id}")
   end
 
   it "displays its all the projects attributes and the challenges attributes" do
-    recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
-    news_chic = @recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
-
     visit "/projects/#{@news_chic.id}"
 
     expect(page).to have_content("News Chic")
     expect(page).to have_content("Newspaper")
     expect(page).to have_content("Recycled Material")
+
+  end
+
+  it "can display total count of contestants for a project" do
+    visit "/projects/#{@news_chic.id}"
+
+    expect(page).to have_content(2)
 
   end
 end
