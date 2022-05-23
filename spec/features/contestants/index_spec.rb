ee63f1a28f79800bab188182213327dd7e96a949
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-05-23 15:28:18 -0600

    Test: can display the average years of experience of contestants for a project

--- spec/features/contestants/index_spec.rb
+++ spec/features/contestants/index_spec.rb
@@ -19,8 +19,7 @@ RSpec.describe 'contestants index page' do
     ContestantProject.create(contestant_id: gretchen.id, project_id: upholstery_tux.id)
 
     visit '/contestants'
-    # save_and_open_page
-# require "pry"; binding.pry
+    
     expect(page).to have_content(jay.name)
     expect(jay.name).to appear_before(jay.projects.name)
   end
--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -42,7 +42,7 @@ RSpec.describe "projects show page" do
     expect(page).to have_content("2")
   end
 
-#   User Story Extension 1 - Average years of experience for contestants by project
+# User Story Extension 1 - Average years of experience for contestants by project
 #
 # As a visitor,
 # When I visit a project's show page
@@ -54,6 +54,21 @@ RSpec.describe "projects show page" do
 #   Average Contestant Experience: 10.25 years)
 
   it "average years of experience for contestants by project" do
+    recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+    furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
 
+    news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+    upholstery_tux = furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+
+    jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+
+    ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+    ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+    ContestantProject.create(contestant_id: gretchen.id, project_id: upholstery_tux.id)
+
+    visit "/projects/#{news_chic.id}"
+
+    expect(page).to have_content("12.5")
   end
 end
