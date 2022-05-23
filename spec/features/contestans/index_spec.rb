8caefba1ebe9582f2561155de15e66b43a679929
Zachary P <zachary.s.prince@outlook.com>
2022-05-23 13:07:11 -0400

    Add contestant index test

---
+++ spec/features/contestans/index_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.describe 'Contestants index page' do
+  it 'Shows all contestants names and projects they are on' do
+    recycled_material_challenge = Challenge.create(theme: 'Recycled Material', project_budget: 1000)
+    furniture_challenge = Challenge.create(theme: 'Apartment Furnishings', project_budget: 1000)
+
+    news_chic = recycled_material_challenge.projects.create(name: 'News Chic', material: 'Newspaper')
+    boardfit = recycled_material_challenge.projects.create(name: 'Boardfit', material: 'Cardboard Boxes')
+
+    upholstery_tux = furniture_challenge.projects.create(name: 'Upholstery Tuxedo', material: 'Couch')
+    lit_fit = furniture_challenge.projects.create(name: 'Litfit', material: 'Lamp')
+
+    jay = Contestant.create(name: 'Jay McCarroll', age: 40, hometown: 'LA', years_of_experience: 13)
+    gretchen = Contestant.create(name: 'Gretchen Jones', age: 36, hometown: 'NYC', years_of_experience: 12)
+    kentaro = Contestant.create(name: 'Kentaro Kameyama', age: 30, hometown: 'Boston', years_of_experience: 8)
+    erin = Contestant.create(name: 'Erin Robertson', age: 44, hometown: 'Denver', years_of_experience: 15)
+
+    ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+    ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+    ContestantProject.create(contestant_id: gretchen.id, project_id: upholstery_tux.id)
+    ContestantProject.create(contestant_id: kentaro.id, project_id: upholstery_tux.id)
+    ContestantProject.create(contestant_id: kentaro.id, project_id: boardfit.id)
+    ContestantProject.create(contestant_id: erin.id, project_id: boardfit.id)
+
+    visit '/contestants'
+
+    within("div##{jay.id}") do
+      expect(jay.name).to appear_before(news_chic.name)
+    end
+
+    within("div##{gretchen.id}") do
+      expect(gretchen.name).to appear_before(news_chic.name)
+
+      expect(gretchen.name).to appear_before(upholstery_tux.name)
+    end
+
+    within("div##{kentaro.id}") do
+      expect(kentaro.name).to appear_before(upholstery_tux.name)
+
+      expect(kentaro.name).to appear_before(boardfit.name)
+    end
+
+    within("div##{erin.id}") do
+      expect(erin.name).to appear_before(boardfit.name)
+    end
+  end
+end
