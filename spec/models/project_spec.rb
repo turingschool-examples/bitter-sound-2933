79810ae8cf1357aa84fda31a5303b377088ef400
Wesley Garcia <wes@alltradestech.com>
2022-05-23 14:08:37 -0700

    Add test for Project.challenge_theme

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -12,4 +12,34 @@ RSpec.describe Project, type: :model do
     it {should have_many :contestant_projects}
     it {should have_many(:contestants).through(:contestant_projects)}
   end
+
+  describe ".instance methods" do
+    let!(:recycled_material_challenge) { Challenge.create(theme: "Recycled Material", project_budget: 1000) }
+    let!(:furniture_challenge) { Challenge.create(theme: "Apartment Furnishings", project_budget: 1000) }
+
+    let!(:news_chic) { recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper") }
+    let!(:boardfit) { recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes") }
+
+    let!(:upholstery_tux) { furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch") }
+    let!(:lit_fit) { furniture_challenge.projects.create(name: "Litfit", material: "Lamp") }
+
+    let!(:jay) { Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13) }
+    let!(:gretchen) { Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12) }
+    let!(:kentaro) { Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8) }
+    let!(:erin) { Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15) }
+
+    before :each do
+      ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+      ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+      ContestantProject.create(contestant_id: gretchen.id, project_id: upholstery_tux.id)
+      ContestantProject.create(contestant_id: kentaro.id, project_id: upholstery_tux.id)
+      ContestantProject.create(contestant_id: kentaro.id, project_id: boardfit.id)
+      ContestantProject.create(contestant_id: erin.id, project_id: boardfit.id)
+    end
+
+    it ".challenge_theme" do
+      expect(news_chic.challenge_theme).to eq("Recycled Material")
+      expect(upholstery_tux.challenge_theme).to eq("Apartment Furnishings")
+    end
+  end
 end
