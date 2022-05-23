1d17cc04f6a49d444a8897ab1c3683b451c73564
Sarah Hall <swatson1994@yahoo.com>
2022-05-23 19:36:39 -0400

    "Add bones feature test for contestant count on project show page."

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -15,4 +15,21 @@ RSpec.describe 'Projects show page', type: :feature do
     expect(page).to_not have_content(boardfit.name)
     expect(page).to_not have_content("Material: #{boardfit.material}")
   end
+
+  it 'can display how many contestants worked on the project' do
+    recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+
+    news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+    boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+
+    jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+
+    ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+    ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+
+    visit "/projects/#{news_chic.id}"
+
+    expect(page).to have_content("Number of Contestants: 2")
+  end
 end
