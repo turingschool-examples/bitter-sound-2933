eae98de8ff1711fb826104d666ea2c760777fb3c
Saba Bhamidipati <saba.bhamidipati@gmail.com>
2022-05-23 10:03:58 -0400

    test: count contestants on projects

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -11,6 +11,17 @@ RSpec.describe 'project show page' do
     @upholstery_tux = @furniture_challenge.projects.create!(name: "Upholstery Tuxedo", material: "Couch")
     @lit_fit = @furniture_challenge.projects.create!(name: "Litfit", material: "Lamp")
 
+    @jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    @gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+    @kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+    @erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+
+    ContestantProject.create(contestant_id: @jay.id, project_id: @news_chic.id)
+    ContestantProject.create(contestant_id: @gretchen.id, project_id: @news_chic.id)
+    ContestantProject.create(contestant_id: @gretchen.id, project_id: @upholstery_tux.id)
+    ContestantProject.create(contestant_id: @kentaro.id, project_id: @upholstery_tux.id)
+    ContestantProject.create(contestant_id: @kentaro.id, project_id: @boardfit.id)
+    ContestantProject.create(contestant_id: @erin.id, project_id: @boardfit.id)\
   end
 
   it 'shows project name and material' do
@@ -19,5 +30,11 @@ RSpec.describe 'project show page' do
     expect(page).to have_content("News Chic")
     expect(page).to have_content("Newspaper")
     expect(page).to have_content("Recycled Ma")
+  end
+
+  it 'shows number of contestants on this project' do
+    visit "/projects/#{@news_chic.id}"
+    # require "pry"; binding.pry
+    expect(page).to have_content("Number of contestants: 2")
   end
 end
