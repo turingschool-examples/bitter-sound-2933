3ed27485ce4eea2a231d24f07a10b6c968c08e32
tjhaines-cap <tjhaines@eckerd.edu>
2022-05-22 18:06:17 -0600

    Feat: add update method to projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,4 +3,9 @@ class ProjectsController < ApplicationController
     def show
         @project = Project.find(params[:project_id])
     end
+
+    def update
+        ContestantProject.create(contestant_id: params[:contestant_id], project_id: params[:project_id])
+        redirect_to "/projects/#{params[:project_id]}"
+    end
 end
