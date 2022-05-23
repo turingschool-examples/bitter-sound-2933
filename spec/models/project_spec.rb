ada3dc276ab6c51877986dd60f23da74c2f4f5d7
Zachary P <zachary.s.prince@outlook.com>
2022-05-23 13:54:37 -0400

    Add average experience method test

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -23,5 +23,18 @@ RSpec.describe Project, type: :model do
 
       expect(news_chic.contestant_count).to eq(2)
     end
+
+    it 'can return average years of experience of assigned contestants' do
+      recycled_material_challenge = Challenge.create(theme: 'Recycled Material', project_budget: 1000)
+      news_chic = recycled_material_challenge.projects.create(name: 'News Chic', material: 'Newspaper')
+      jay = Contestant.create(name: 'Jay McCarroll', age: 40, hometown: 'LA', years_of_experience: 13)
+      gretchen = Contestant.create(name: 'Gretchen Jones', age: 36, hometown: 'NYC', years_of_experience: 12)
+      oscar = Contestant.create(name: 'Oscar Grouch', age: 63, hometown: 'NYC', years_of_experience: 44)
+      ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+      ContestantProject.create(contestant_id: joscar.id, project_id: news_chic.id)
+      ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+
+      expect(news_chic.average_exp).to eq(23)
+    end
   end
 end
