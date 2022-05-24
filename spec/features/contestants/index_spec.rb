c1353b26645405914768d7d4efadb7ad366facb0
Eric Espindola <eric_e_2012@hotmail.com>
2022-05-23 17:58:40 -0700

    create feature test for contestant index page

---
+++ spec/features/contestants/index_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+RSpec.describe 'contestants index page', type: :feature do
+  it 'can see all contestants and contestant projects' do
+    recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+    furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+
+    news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+    boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+    upholstery_tux = furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+
+    jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+
+    ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+    ContestantProject.create(contestant_id: kentaro.id, project_id: upholstery_tux.id)
+    ContestantProject.create(contestant_id: kentaro.id, project_id: boardfit.id)
+
+    visit("/contestants")
+
+    expect(page).to have_content("Jay McCarroll")
+    expect(page).to have_content("Litfit")
+    expect(page).to have_content("Rug Tuxedo")
+    expect(page).to have_content("Kentaro Kameyama")
+    expect(page).to have_content("LeatherFeather")
+  end
+end
