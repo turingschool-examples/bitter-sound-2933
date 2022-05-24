8be9d7b564ec05f90abd64f6da2acbc985718d33
Caden Jarrett <caden.jarrett99@gmail.com>
2022-05-24 14:55:17 -0600

    wrote test for average number of contestants

--- spec/features/project_show_spec.rb
+++ spec/features/project_show_spec.rb
@@ -20,6 +20,18 @@ RSpec.describe Project, type: :feature do
 
     @upholstery_tux = @furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
     @lit_fit = @furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+
+    @jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    @gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+    @kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+    @erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+
+    ContestantProject.create(contestant_id: @jay.id, project_id: @news_chic.id)
+    ContestantProject.create(contestant_id: @gretchen.id, project_id: @news_chic.id)
+    ContestantProject.create(contestant_id: @gretchen.id, project_id: @upholstery_tux.id)
+    ContestantProject.create(contestant_id: @kentaro.id, project_id: @upholstery_tux.id)
+    ContestantProject.create(contestant_id: @kentaro.id, project_id: @boardfit.id)
+    ContestantProject.create(contestant_id: @erin.id, project_id: @boardfit.id)
   end
 
   it 'displays the projects name and material on the show page' do
@@ -39,7 +51,8 @@ RSpec.describe Project, type: :feature do
   #   Material: Lamp Shade
   #   Challenge Theme: Apartment Furnishings
   #   Number of Contestants: 3 )
-  # it 'displays the number of contestants on a project' do
-  #   visit ("/projects/#{@news_chic.id}")
-  # end
+  it 'displays the number of contestants on a project' do
+    visit ("/projects/#{@news_chic.id}")
+    expect(page).to have_content('Number of Contestants: 2')
+  end
 end
