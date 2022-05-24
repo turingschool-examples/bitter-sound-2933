521661b46ada3c2dfd22aa5ae9b80b48ba8889c3
Caden Jarrett <caden.jarrett99@gmail.com>
2022-05-24 13:50:48 -0600

    added validations and relationships for Project

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,8 @@
 class Project <ApplicationRecord
-  validates_presence_of :name, :material
+  # binding.pry
+  validates_presence_of :name
+  validates_presence_of :material
   belongs_to :challenge
+  has_many :contestant_projects
+  has_many :contestants, through: :contestant_projects
 end
