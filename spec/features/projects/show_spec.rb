e857a2ae185ae566d5b1c357e1f1d70c5aac38c0
Sage Skaff <skaffsage@gmail.com>
2022-05-24 11:18:26 -0700

    Add number of contestants in project test

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -11,12 +11,35 @@ RSpec.describe 'Projects Show Page' do
     let!(:lit_fit) { furniture_challenge.projects.create!(name: "Litfit", material: "Lamp") }
 
 
-  it "shows project name, material, and theme it belongs to" do
+  it "lists project name, material, and theme it belongs to" do
     visit "/projects/#{news_chic.id}"
 
     expect(page).to have_content(news_chic.name)
     expect(page).to have_content(news_chic.material)
     expect(page).to have_content("Recycled Material")
     expect(page).to_not have_content("Apartment Furnishings")
+  end
+
+  it "lists number of contestants in project" do
+    jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+    kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+    erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+
+
+    ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+    ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+    ContestantProject.create(contestant_id: gretchen.id, project_id: upholstery_tux.id)
+    ContestantProject.create(contestant_id: kentaro.id, project_id: upholstery_tux.id)
+    ContestantProject.create(contestant_id: kentaro.id, project_id: boardfit.id)
+    ContestantProject.create(contestant_id: erin.id, project_id: boardfit.id)
+
+    visit "/projects/#{boardfit.id}"
+
+    expect(page).to have_content("Number of Contestants: 2")
+
+    visit "/projects/#{lit_fit.id}"
+
+    expect(page).to have_content("Number of Contestants: 0")
   end
 end
