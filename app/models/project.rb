f97391b5877510e1af1ff50e99f3e003d2693d53
Eric Espindola <eric_e_2012@hotmail.com>
2022-05-23 17:47:51 -0700

    add feature to view a projects info on project show page

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,8 @@ class Project <ApplicationRecord
   belongs_to :challenge
   has_many :contestant_projects
   has_many :contestants, through: :contestant_projects
+
+  def project_theme
+    self.challenge.theme
+  end
 end
