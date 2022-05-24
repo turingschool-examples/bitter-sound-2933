be25dbb180893d2629b6f4bcb0b0d72e743ac955
Eric Espindola <eric_e_2012@hotmail.com>
2022-05-23 18:39:34 -0700

    add test to count number of contestants on a project

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -19,5 +19,19 @@ RSpec.describe Project, type: :model do
     lit_fit = furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
     expect(lit_fit.project_theme).to eq("Apartment Furnishings")
     end
+
+    it "can count number of contestants on project" do
+      recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+      news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+      boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+      jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+      gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+      kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+      erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+      ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+      ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+      ContestantProject.create(contestant_id: kentaro.id, project_id: news_chic.id)
+    expect(news_chic.num_contestants).to eq(3)
+    end
   end
 end
