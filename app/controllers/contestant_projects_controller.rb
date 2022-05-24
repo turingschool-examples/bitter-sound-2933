49a016cad9e4742846cbe171f6bec2049d133035
Sarah Hall <swatson1994@yahoo.com>
2022-05-23 20:38:59 -0400

    "Final commit"

--- app/controllers/contestant_projects_controller.rb
+++ app/controllers/contestant_projects_controller.rb
@@ -4,5 +4,4 @@ class ContestantProjectsController < ApplicationController
     ContestantProject.create(contestant_id: params[:contestant_id], project_id: params[:project_id])
     redirect_to "/projects/#{params[:project_id]}"
   end
-
 end
