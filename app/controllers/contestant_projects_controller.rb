29b928e8b75a9f23a2ee01eca95bd81cd31f66bd
Sarah Hall <swatson1994@yahoo.com>
2022-05-23 20:35:17 -0400

    "Add route and controller for creating ContestantProjects"

---
+++ app/controllers/contestant_projects_controller.rb
@@ -0,0 +1,8 @@
+class ContestantProjectsController < ApplicationController
+
+  def create
+    ContestantProject.create(contestant_id: params[:contestant_id], project_id: params[:project_id])
+    redirect_to "/projects/#{params[:project_id]}"
+  end
+
+end
