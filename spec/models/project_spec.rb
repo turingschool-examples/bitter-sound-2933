64002f07ba838124fb721ce9b654391033d822f3
Eric Espindola <eric_e_2012@hotmail.com>
2022-05-23 17:47:08 -0700

    create project_theme method to get a projects challenge theme

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -12,4 +12,12 @@ RSpec.describe Project, type: :model do
     it {should have_many :contestant_projects}
     it {should have_many(:contestants).through(:contestant_projects)}
   end
+
+  describe "instance methods" do
+    it "can get a projects theme" do
+    furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+    lit_fit = furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+    expect(lit_fit.project_theme).to eq("Apartment Furnishings")
+    end
+  end
 end
